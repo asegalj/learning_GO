22ed2bed3ae320ff2c8ba8823ceb865efae86138
Amir Segal <amir.segal13@gmail.com>
2019-07-11 12:46:39 +0300

    before functions

--- mapsword.go
+++ mapsword.go
@@ -5,18 +5,34 @@ import (
 	"strings"
 )
 
+//My solution
+// func main() {
+// 	wordMapper := map[string]int{}
+// 	text := `Needles and pins
+// 			 Needles and Pins
+// 			 sew me Sail
+// 			 tO catCH me the wind`
+// 	b := strings.ToLower(text)
+// 	a := strings.Fields(b)
+// 	fmt.Println(a)
+// 	for _, value := range a {
+// 		// fmt.Println(wordMapper[value]=wordMapper[value]+1)
+// 		wordMapper[value]++
+// 	}
+// 	fmt.Println(wordMapper)
+// }
+
 func main() {
-	wordMapper := map[string]int{}
+
 	text := `Needles and pins 
 			 Needles and Pins
 			 sew me Sail
 			 tO catCH me the wind`
-	b := strings.ToLower(text)
-	a := strings.Fields(b)
-	fmt.Println(a)
-	for _, value := range a {
+	words := strings.Fields(text)
+	wordMapper := map[string]int{}
+	for _, word := range words {
 		// fmt.Println(wordMapper[value]=wordMapper[value]+1)
-		wordMapper[value] = wordMapper[value] + 1
+		wordMapper[strings.ToLower(word)]++
 	}
 	fmt.Println(wordMapper)
 }
